e47ba56a4b4bae7f15ee68caa3dacd5fb912bfdc
minsikmoon <msmoon.business@gmail.com>
2018-10-29 23:30:07 +0900

    custom시작

--- src/main/java/listener/AppServletContextListener.java
+++ src/main/java/listener/AppServletContextListener.java
@@ -1,10 +1,7 @@
 package listener;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import data.container.DataMapContainer;
 
 public class AppServletContextListener implements ServletContextListener {
 
@@ -14,11 +11,7 @@ public class AppServletContextListener implements ServletContextListener {
 	}
 
 	public void contextInitialized(ServletContextEvent event) {
-		//여기서 서블릿 컨텍스트 준비시켜준다. 
-		ServletContext sc = event.getServletContext();
-		DataMapContainer dmc = new DataMapContainer();
-		sc.setAttribute("dmc", dmc);
-		System.out.println("서블릿 컨텍스트 장착완료~~");
+		System.out.println("시작됩니다.");
 	}
 
 }
